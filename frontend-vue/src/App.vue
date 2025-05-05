cfd2583a99210be8caa2c9efdb5675a34370324c
Dillon Steyl <dillon.steyl@snazzyshrimp.com>
2025-05-05 23:10:35 +1000

    setup daisyUI

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -3,20 +3,22 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="flex justify-center">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-col min-h-[100vh] justify-center m-0">
+    <div class="flex justify-center">
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
 .logo {
-  height: 8em;
+  height: 12em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
