e8a53c11ad495957843000bffa66e6201ff08d9b
Anna Henderson <anna.henderson410@gmail.com>
2025-05-06 16:59:28 +1000

    addressing comments

--- backend/tickit/serializers.py
+++ backend/tickit/serializers.py
@@ -2,19 +2,12 @@ from tickit.models import Item, ItemList
 from rest_framework import serializers
 
 class ItemListSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=30, required=True)
-
     class Meta:
         model = ItemList
         fields = ['id', 'name']
 
 
 class ItemSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    text = serializers.CharField(max_length=100, required=True)
-    is_completed = serializers.BooleanField(default=False)
-    item_list = serializers.PrimaryKeyRelatedField(queryset=ItemList.objects.all())
     item_list_details = ItemListSerializer(source='item_list', read_only=True)
 
     class Meta:
