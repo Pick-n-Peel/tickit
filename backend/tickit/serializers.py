53f858729b0f4686b43391c7c4f79dab7c437b48
Anna Henderson <anna.henderson410@gmail.com>
2025-05-06 19:29:12 +1000

    fix formatting with pre-commit

--- backend/tickit/serializers.py
+++ backend/tickit/serializers.py
@@ -1,15 +1,16 @@
 from tickit.models import Item, ItemList
 from rest_framework import serializers
 
+
 class ItemListSerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemList
-        fields = ['id', 'name']
+        fields = ["id", "name"]
 
 
 class ItemSerializer(serializers.ModelSerializer):
-    item_list_details = ItemListSerializer(source='item_list', read_only=True)
+    item_list_details = ItemListSerializer(source="item_list", read_only=True)
 
     class Meta:
         model = Item
-        fields = ['id', 'text','is_completed','item_list', 'item_list_details']
+        fields = ["id", "text", "is_completed", "item_list", "item_list_details"]
