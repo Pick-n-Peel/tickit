402fd660c4be09d47a0bedd04f6c5d0268dbfac2
Anna Henderson <anna.henderson410@gmail.com>
2025-05-07 22:24:11 +1000

    add reordering to the orders based on FE request

--- backend/tickit/serializers.py
+++ backend/tickit/serializers.py
@@ -2,15 +2,6 @@ from tickit.models import Item, ItemList
 from rest_framework import serializers
 
 
-class OrderDefault:
-    requires_context = True
-
-    def __call__(self, serializer_field):
-        data = serializer_field.context.get("request").data
-        item_list = data.get("item_list")
-        return Item.objects.all().filter(item_list=item_list).count()
-
-
 class ItemListSerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemList
@@ -18,8 +9,13 @@ class ItemListSerializer(serializers.ModelSerializer):
 
 
 class ItemSerializer(serializers.ModelSerializer):
-    order = serializers.IntegerField(default=OrderDefault())
+    order = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Item
         fields = ["id", "text", "is_completed", "item_list", "order"]
+
+    def create(self, validated_data):
+        item_list = validated_data["item_list"]
+        validated_data["order"] = Item.objects.filter(item_list=item_list).count()
+        return super().create(validated_data)
--- backend/tickit/views.py
+++ backend/tickit/views.py
@@ -1,6 +1,8 @@
 from tickit.models import Item, ItemList
 from tickit.serializers import ItemSerializer, ItemListSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ItemListViewSet(viewsets.ModelViewSet):
@@ -11,3 +13,32 @@ class ItemListViewSet(viewsets.ModelViewSet):
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+
+    @action(detail=True, methods=["put"], name="reorder")
+    def reorder(self, request, *args, **kwargs):
+        new_order = request.data.get("new_order")
+
+        item_being_reordered = self.get_object()
+        item_list = item_being_reordered.item_list
+        current_order = item_being_reordered.order
+
+        if new_order > current_order:
+            items_to_reorder = self.queryset.all().filter(
+                item_list=item_list, order__lte=new_order, order__gt=current_order
+            )
+            adjustment = -1
+
+        elif new_order < current_order:
+            items_to_reorder = self.queryset.all().filter(
+                item_list=item_list, order__gte=new_order, order__lt=current_order
+            )
+            adjustment = 1
+
+        for item in items_to_reorder:
+            item.order += adjustment
+            item.save()
+
+        item_being_reordered.order = new_order
+        item_being_reordered.save()
+
+        return Response({"status": "items reordered"})
