d3e3fd07b265d117332dec16bfca3527fa525dff
Anna Henderson <anna.henderson410@gmail.com>
2025-05-10 14:43:09 +1000

    add filtering to item viewset

--- backend/tickit/views.py
+++ backend/tickit/views.py
@@ -1,6 +1,7 @@
 from tickit.models import Item, ItemList
 from tickit.serializers import ItemSerializer, ItemListSerializer
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ItemListViewSet(viewsets.ModelViewSet):
@@ -11,3 +12,5 @@ class ItemListViewSet(viewsets.ModelViewSet):
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["item_list"]
